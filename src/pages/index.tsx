f6fe81ecb7c5a7a2badb0723f77531dd5474a40f
Luis Jonson <luislindonjonson@yahoo.br>
2024-04-30 21:52:58 -0300

    Aula 03 - Curso de React Next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,21 @@
 
 // const inter = Inter({ subsets: ["latin"] });
 
+const name = 'Jonson'
+let canal = 'CBFCursos'
+
+function retnome(){
+  return name
+}
+
 export default function Home() {
   return (
     <main>
+      <div>{retnome()}</div>
+      <div>{canal}</div>
+      <div>Cursos de react Next </div>
+      <div>Typescript</div>
+      <div>React</div>
     </main>
   );
 }
