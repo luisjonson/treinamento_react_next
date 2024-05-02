5e88c203ef1b29c82661edbb3f11ba79b0132d1e
Luis Jonson <luislindonjonson@yahoo.br>
2024-05-01 21:34:32 -0300

    Aula 05 - Curso de React Next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,12 @@ const cnl = ()=>{
 
 function Topo(){
   return (
-    <div>
+    <div className='flex justify-between items-center bg-zinc-300 h-[100px]'>
       <div>Log</div>
-      <div>{canal}</div>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='text-3x1'>{canal}</div>
+        <div className='subtituloTopo'>Curso de react</div>
+      </div>
       <div>{name}</div>
     </div>
   )
