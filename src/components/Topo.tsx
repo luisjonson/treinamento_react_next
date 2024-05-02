ed99c88ed65cfba8a474d6bb3cb45a3315085d9d
Luis Jonson <luislindonjonson@yahoo.br>
2024-05-01 22:25:29 -0300

    Criando COMPONENTES #P1 em React Next - Aula 07 - Curso de React Next Typescript

---
+++ src/components/Topo.tsx
@@ -0,0 +1,12 @@
+export default function Topo(){
+    return (
+        <div className='flex justify-between items-center bg-zinc-300 h-[100px]'>
+          <div>Log</div>
+          <div className='flex flex-col justify-center items-center'>
+            <div className='text-3x1'>CFB Cursos</div>
+            <div className='subtituloTopo'>Curso de react</div>
+          </div>
+          <div>Jonson</div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@
 // import { Inter } from "next/font/google";
 
 import { getDisplayName } from "next/dist/shared/lib/utils"
+import Topo from "@/components/Topo"
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -14,19 +15,6 @@ function retnome(){
 
 const cnl = ()=>{
   return canal
-}
-
-function Topo(){
-  return (
-    <div className='flex justify-between items-center bg-zinc-300 h-[100px]'>
-      <div>Log</div>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='text-3x1'>{canal}</div>
-        <div className='subtituloTopo'>Curso de react</div>
-      </div>
-      <div>{name}</div>
-    </div>
-  )
 }
 
 export default function Home() {
